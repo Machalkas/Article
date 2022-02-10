ac20ed0c10dc7fa7d74b792d7666912e951810a3
Machalka <al1999dk@gmail.com>
2022-02-10 22:28:56 +0300

    подготовка к деплою

--- articweb/settings.py
+++ articweb/settings.py
@@ -24,7 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+SESSION_COOKIE_SECURE=True
+CSRF_COOKIE_SECURE=True
+
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
@@ -131,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
